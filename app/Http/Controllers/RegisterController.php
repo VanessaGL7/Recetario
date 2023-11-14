85dc90368fdd92c802b58e1a5c0223af5e90e695
VanessaGL7 <glvanessa2@gmail.com>
2023-11-14 14:32:30 -0600

    i

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,19 +6,35 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Passport\HasApiTokens;
+use Validator;
 
 class RegisterController extends Controller
 {
     public function register(Request $request)
     {
-        $input = $request->all();
-        $input['password'] = bcrypt($input['password']);
-        $user = User::create($input);
-        $token = $user->createToken('MyApp')->accessToken;
+        $validator=Validator::make($request->all(),[
+            'name'=>'required',
+            'email'=>'required|email',
+            'password'=>'required',
+            'c_password'=>'required|same:password',
+        ]);
 
+        if($validator->fails()){
+            return $this->sendError('Validation Error.',
+            $validator->errors());
+        }
+
+        $input=$request->all();
+        $input['password']=bcrypt($input['password']);
+        $user=User::create($input);
+        $success['token']=$user->createToken('MyApp')->accessToken;
+        $success['name']=$user->name;
+        return $this->sendResponse($success,
+        'User Register successfully');
+       
         
-        // Devolver el token como respuesta
-        return response()->json(['token' => $token, 'name' => $user->name], 200);
+        /* Devolver el token como respuesta
+        return response()->json(['token' => $token, 'name' => $user->name], 200);*/
     }
 
     public function login(Request $request)
