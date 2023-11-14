1ff80a348c576ea2d6b3036b0005ddf88a07d51a
RicardoGP12 <20150411@aguascalientes.tecnm.mx>
2023-11-14 11:24:31 -0600

    a

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import Main from './components/Main';
 import { BrowserRouter } from "react-router-dom";
+import './bootstrap';
+import '../css/app.css'
+
 
 
 ReactDOM.createRoot(document.getElementById("example")).render(
   <BrowserRouter>
-    <Main/>
+    <Main></Main>
   </BrowserRouter>
 );
